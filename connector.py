71c7c5d9289cba4146fee0a0772a95402a3ada38
Ashutosh A. Patwardhan <ashutosh.patwardhan@iitbombay.org>
2021-05-25 19:53:55 -0500

    feat: Allow flow rate and temperature differences between the two streams
    The flow rate and/or temperature of the in and out streams can differ by a specified amount. This allows modeling of leaks, heat loss or heat gain.

--- connector.py
+++ connector.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 Makes a connection between two streams.
-The flow rate, temperaature and each component fraction of one Stream object
-are specified to be equal to the corresponding attributes of the second Stream
-object.
+The flow rates of the two streams can be equal, or differ by a specified 
+amount (positive or negative). Can be used to model a leak, but the leak
+is lost - it is not captured by another stream.
+The temperatures of the two streams can be equal, or differ by a specified
+amount (positive or negative). Can be used to model a heat leak.
+The fraction of each component has to be the same for each stream.
 """
 
 from unit import Unit
@@ -12,7 +15,8 @@ class Connector(Unit):
     
     n_connectors = 0
     
-    def __init__(self, stream_in, stream_out, name=None):
+    def __init__(self, stream_in, stream_out, flow_diff=0, temp_diff=0, 
+                 name=None):
         self.connector_num = Connector.n_connectors
         super().__init__(name, self.connector_num)
         Connector.n_connectors += 1
@@ -20,6 +24,8 @@ class Connector(Unit):
         self.stream_out = stream_out
         self.n_vars = 0
         self.n_eqns = self.stream_in.n_comps + 2
+        self.flow_diff = flow_diff
+        self.temp_diff = temp_diff
 
     def __str__(self):
         s = ''
@@ -36,9 +42,9 @@ class Connector(Unit):
     
     def calculate(self):
         # equate flow: 1 equation
-        self.eqns[0] = self.stream_in.xvar[0] - self.stream_out.xvar[0]
+        self.eqns[0] = self.stream_in.xvar[0] + self.flow_diff - self.stream_out.xvar[0]
         # equate temperature: 1 equation
-        self.eqns[1] = self.stream_in.xvar[1] - self.stream_out.xvar[1]
+        self.eqns[1] = self.stream_in.xvar[1] + self.temp_diff - self.stream_out.xvar[1]
         # equate component fractions: n_comps equations
         for i_comp in range(self.stream_in.n_comps):
             self.eqns[2 + i_comp] = self.stream_in.xvar[2 + i_comp] - self.stream_out.xvar[2 + i_comp]
