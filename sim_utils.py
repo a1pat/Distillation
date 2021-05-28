98d861f4ca8b5b618dc8c8474e98628f6c3b7f1a
Ashutosh A. Patwardhan <ashutosh.patwardhan@iitbombay.org>
2021-05-27 22:46:36 -0500

    feat: Added function to print information about each component in unit_dict.

--- sim_utils.py
+++ sim_utils.py
@@ -26,6 +26,7 @@ def map_var_to_unit1(x, unit_dict):
             n_vars = unit_dict[u].n_vars
             if n_vars > 0:
                 unit_dict[u].xvar = x[idx:idx+n_vars]
+                #print(u, idx, idx+n_vars-1)
                 idx += n_vars
             idx = map_var_to_unit2(x, unit_dict[u].unit_dict, idx)
         return idx
@@ -56,6 +57,8 @@ def map_eqn_to_unit1(e, unit_dict):
             n_eqns = unit_dict[u].n_eqns
             if n_eqns > 0:
                 unit_dict[u].eqns = e[idx:idx+n_eqns]
+#                e[idx:idx+n_eqns] = unit_dict[u].eqns
+                #print(u, idx, idx+n_eqns-1)
                 idx += n_eqns
             idx = map_eqn_to_unit2(e, unit_dict[u].unit_dict, idx)
         return idx
@@ -88,3 +91,33 @@ def process_eqns(xvar, unit_dict, eqns):
     return eqns.tolist()
 
 
+def get_info(unit_dict):
+    """
+    Print model info.
+    Parameters
+    ----------
+    unit_dict : dict
+        dictionary containing all Unit objects in the simulation.
+
+    Returns
+    -------
+    info : str
+        information about the unit_dict
+
+    """
+
+    def get_info2(unit_dict, info):
+        info = info + 'unit_dict contains {}'.format(list(unit_dict.keys()))
+        for u in sorted(unit_dict.keys()):
+            info = info + '\n' + str(unit_dict[u])
+            if not unit_dict[u].unit_dict:
+                info = info + u + ' has NO unit_dict \n'
+            else:
+                info = info + u + ' has a unit_dict with ' + str(len(unit_dict[u].unit_dict.keys())) + ' items\n'
+                info = info + 'entering recursion\n'
+                info = get_info2(unit_dict[u].unit_dict, info)
+                info = info + 'exited recursion\n'
+        return info
+
+    info = get_info2(unit_dict, '')        
+    return info
