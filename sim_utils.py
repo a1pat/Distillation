c24f65609269a01ee2b6bf740e1712531071919b
Ashutosh A. Patwardhan <ashutosh.patwardhan@iitbombay.org>
2021-06-17 21:33:40 -0500

    feat: Added function to get an array containing the variables of each Unit object in unit_dict
    Also, sorted the keys when looping through a dict to ensure order.

--- sim_utils.py
+++ sim_utils.py
@@ -3,6 +3,8 @@
 Utility functions used in the running of a simulation.
 """
 
+import numpy as np
+
 def map_var_to_unit1(x, unit_dict):
     """
     Map simulation variable array to the variables in each Unit object
@@ -22,11 +24,11 @@ def map_var_to_unit1(x, unit_dict):
 
     def map_var_to_unit2(x, unit_dict, idx):
         
-        for u in unit_dict:
+        for u in sorted(unit_dict.keys()):
             n_vars = unit_dict[u].n_vars
             if n_vars > 0:
+                #print(type(x))
                 unit_dict[u].xvar = x[idx:idx+n_vars]
-                #print(u, idx, idx+n_vars-1)
                 idx += n_vars
             idx = map_var_to_unit2(x, unit_dict[u].unit_dict, idx)
         return idx
@@ -53,7 +55,7 @@ def map_eqn_to_unit1(e, unit_dict):
     """
 
     def map_eqn_to_unit2(e, unit_dict, idx):
-        for u in unit_dict:
+        for u in sorted(unit_dict.keys()):
             n_eqns = unit_dict[u].n_eqns
             if n_eqns > 0:
                 unit_dict[u].eqns = e[idx:idx+n_eqns]
@@ -86,7 +88,7 @@ def process_eqns(xvar, unit_dict, eqns):
 
     """
     map_var_to_unit1(xvar, unit_dict)
-    for u in unit_dict:
+    for u in sorted(unit_dict.keys()):
         unit_dict[u].calculate()
     return eqns.tolist()
 
@@ -139,7 +141,7 @@ def get_num_vars(unit_dict):
 
     """
     nvars = 0
-    for u in unit_dict:
+    for u in sorted(unit_dict.keys()):
         nvars += unit_dict[u].num_vars()
     return nvars
 
@@ -160,8 +162,42 @@ def get_num_eqns(unit_dict):
 
     """
     neqns = 0
-    for u in unit_dict:
+    for u in sorted(unit_dict.keys()):
         neqns += unit_dict[u].num_eqns()
     return neqns
 
 
+def get_unit_vars(unit_dict):
+    """
+    Get variable array of all the variables in each Unit object
+    in unit_dict.
+    Parameters
+    ----------
+    unit_dict : dict
+        dictionary containing all Unit objects in the simulation.
+
+    Returns
+    -------
+    x : array
+        variable array used by the nonlinear equation solver.
+
+    """
+
+    def get_unit_vars_inner(x, unit_dict, idx):
+        
+        for u in sorted(unit_dict.keys()):
+            n_vars = unit_dict[u].n_vars
+            if n_vars > 0:
+                #print(type(x))
+                for i in range(n_vars):
+                    x[idx+i] = unit_dict[u].xvar[i]
+                idx += n_vars
+            x, idx = get_unit_vars_inner(x, unit_dict[u].unit_dict, idx)
+        return x, idx
+
+    x = np.zeros(get_num_vars(unit_dict), dtype=np.float64)
+    idx = 0
+    x, idx = get_unit_vars_inner(x, unit_dict, idx)
+    return x
+    
+    
